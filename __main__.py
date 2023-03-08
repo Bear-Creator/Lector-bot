1433b9867c40914531fe7afd569f1498a8928259
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2023-03-09 00:54:23 +0700

    Added DB

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
 from webserver import keep_alive
 import discord
 from discord.ext import commands
+import sqlite3
 from config import config
 
 
+db = sqlite3.connect('server.db')
+cursor = db.cursor()
 bot = commands.Bot(command_prefix=config.BOT_PREFIX,
                    intents=discord.Intents.all())
 
@@ -15,12 +18,29 @@ if __name__=="__main__":
         exit
 
 
+async def guild_reg(guilde: discord.Guild):
+    cursor.execute(
+
+    )
+
+
 @bot.event
 async def on_ready():
     print(config.STARTUP_MESSAGE)
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"Число слушателей {0}"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"Введите {config.BOT_PREFIX}help"))
+
     for guild in bot.guilds:
         
+        cursor.execute(
+            f'''CREATE TABLE if NOT EXISTS users (
+            id          int NOT NULL,
+            username    varchar(80)
+            PRIMARY KEY (id)
+            );'''
+        )
+
+        db.commit()
+
         print(f"Joined {guild.name}")
 
     print(config.STARTUP_COMPLETE_MESSAGE)

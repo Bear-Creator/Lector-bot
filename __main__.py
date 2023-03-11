73b738470ca291662903b10cc2f27fe1cd88f19e
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2023-03-11 15:24:21 +0700

    Added user insert def

--- __main__.py
+++ __main__.py
@@ -3,12 +3,28 @@ import discord
 from discord.ext import commands
 import sqlite3
 from config import config
+from lib.mytools import *
 
 
-db = sqlite3.connect('server.db')
+db = sqlite3.connect(config.DB_LINK_SERVER)
 cursor = db.cursor()
 bot = commands.Bot(command_prefix=config.BOT_PREFIX,
                    intents=discord.Intents.all())
+
+
+async def new_user(member: discord.Member):
+
+    if (cursor.execute(
+        f'SELECT id FROM users_{member.guild.id} WHERE id == {member.id};').fetchone() is not None
+        ) or (member == bot.user):
+        return
+    
+    cursor.execute(f'INSERT INTO users_{member.guild.id} (id)VALUES ({member.id});')
+    db.commit()
+
+    if (member.guild.id != config.GUILDE_ID): #Костыль - бот не может получить reg беседу для каждой гильдии
+        return
+    await bot.get_channel(config.REGISTRATION_CHANNEL).send(f'<@{member.id}>, пройдите регистрацию. Отправьте {config.BOT_PREFIX}reg')
 
 
 if __name__=="__main__":
@@ -17,11 +33,7 @@ if __name__=="__main__":
         print("Error: No bot token!")
         exit
 
-
-async def guild_reg(guilde: discord.Guild):
-    cursor.execute(
-
-    )
+    copy_table(config.DB_LINK_STUDLIST, config.DB_LINK_SERVER, 'students')
 
 
 @bot.event
@@ -30,19 +42,20 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"Введите {config.BOT_PREFIX}help"))
 
     for guild in bot.guilds:
-        
+        print(f"Joined {guild.name}", guild.id)
+
         cursor.execute(
-            f'''CREATE TABLE if NOT EXISTS users (
-            id          int NOT NULL,
-            username    varchar(80)
-            PRIMARY KEY (id)
+            f'''CREATE TABLE if NOT EXISTS users_{guild.id} (
+            id        int,
+            sid       int,
+            FOREIGN KEY (sid) REFERENCES students (id)
             );'''
         )
-
+        
+        for member in guild.members:
+            await new_user(member)
         db.commit()
-
-        print(f"Joined {guild.name}")
-
+    
     print(config.STARTUP_COMPLETE_MESSAGE)
 
 
